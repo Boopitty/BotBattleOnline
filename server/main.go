c0e657b8641752e9bb22b324660b94acb64e157f
Emil Auguste <emilrauguste@gmail.com>
2026-06-29 15:57:57 -0400

    Add server configuration and environment setup; create initial HTML and .gitignore

--- server/main.go
+++ server/main.go
@@ -2,8 +2,50 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	fmt.Println("Hello from the backend!")
+	fmt.Println("Starting Server")
+	// Load environment variables from .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Could not load godotenv %v", err)
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatalln("PORT env variable not found.")
+	}
+
+	filepathRoot := os.Getenv("FILEPATH_ROOT")
+	if filepathRoot == "" {
+		log.Fatalln("FILEPATH_ROOT env variable not found.")
+	}
+
+	// Mutex for the server
+	mux := http.NewServeMux()
+	appHandler := http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))
+
+	// Handle the front page
+	mux.Handle("/app/", appHandler)
+
+	// create the server object
+	srv := http.Server{
+		Handler:      mux,
+		Addr:         ":" + port,
+		WriteTimeout: 30 * time.Second,
+		ReadTimeout:  30 * time.Second,
+	}
+
+	// this blocks forever, until the server
+	// has an unrecoverable error
+	fmt.Printf("server started on %v\n", port)
+	err = srv.ListenAndServe()
+	log.Fatal(err)
 }
